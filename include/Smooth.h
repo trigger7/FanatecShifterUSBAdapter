ed0dae4591f2242519b6c96806e47e167c2fd3b9
trigger7 <trigger.hikigane@gmail.com>
2021-03-07 16:09:09 +0100

    Fixed smoothing logic

--- include/Smooth.h
+++ include/Smooth.h
@@ -9,28 +9,29 @@ class Smooth
     // Double Exponential Smoothing
     // https://www.itl.nist.gov/div898/handbook/pmc/section4/pmc433.htm
   public:
-    Smooth(uint8_t pin, float new_weight = 0.1, float speed_weight = 0.5)
+    Smooth(uint8_t pin, float alpha = 0.1, float gamma = 0.5)
     {
       _pin = pin;
-      _new_weight = new_weight;
-      _speed_weight = speed_weight;
+      _alpha = alpha;
+      _gamma = gamma;
     }
     uint16_t read()
     {
       _raw_value = analogRead(_pin);
-      _smoothed = _new_weight * _raw_value + (1 - _new_weight) * (_smoothed + _diff_speed);
-      _diff_speed = _speed_weight * (_smoothed - _old_value) + (1 - _speed_weight) * _diff_speed;
-      _old_value = _smoothed;
-      return _smoothed;
+      _smoothed = _alpha * _raw_value + (1. - _alpha) * (_smoothed + _beta);
+      _beta = _gamma * (_smoothed - _old_smoothed) + (1. - _gamma) * _beta;
+      _old_smoothed = _smoothed;
+      // smoothed value can overshoot and become negative, overflowing our unsigned int
+      return max(0, _smoothed);
     }
 
   private:
     uint8_t _pin;
-    float _new_weight;
-    float _speed_weight;
-    uint16_t _raw_value = 2048;
-    uint16_t _smoothed = 2048;
-    uint16_t _diff_speed = 0;
-    uint16_t _old_value = 2048;
+    float _alpha;
+    float _gamma;
+    int16_t _beta = 0;
+    uint16_t _raw_value = 0;
+    int16_t _smoothed = 0;
+    int16_t _old_smoothed = 0;
 };
 #endif
